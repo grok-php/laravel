4251f6e776eb7d7e54e53fe2ab0301840b748b82
Muhammed ElFeqy <thefeqy@gmail.com>
2025-02-25 01:57:13 +0200

    test: Default configuration test

--- tests/Unit/GrokConfigTest.php
+++ tests/Unit/GrokConfigTest.php
@@ -10,6 +10,23 @@ class GrokConfigTest extends TestCase
 {
     public function test_it_uses_default_values_when_not_overridden()
     {
-        // soon ...
+        $config = new GrokConfig(apiKey: 'test-api-key');
+
+        $this->assertEquals('test-api-key', $config->apiKey);
+        $this->assertEquals(DefaultConfig::BASE_URI->value, $config->baseUri);
+        $this->assertEquals((int) DefaultConfig::TIMEOUT->value, $config->timeout);
+    }
+
+    public function test_it_allows_custom_values()
+    {
+        $customBaseUri = 'https://custom-api.grok.dev';
+
+        $config = new GrokConfig(
+            apiKey: 'test-api-key',
+            baseUri: $customBaseUri,
+        );
+
+        $this->assertEquals('test-api-key', $config->apiKey);
+        $this->assertEquals($customBaseUri, $config->baseUri);
     }
 }
