6eea4db609f2b766e46a0e1038e00e1c498bd27f
Muhammed ElFeqy <thefeqy@gmail.com>
2025-02-25 00:59:06 +0200

    chore: add support for vision models

---
+++ src/Services/GrokVision.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace GrokPHP\Laravel\Services;
+
+use GrokPHP\Client\Clients\GrokClient;
+use GrokPHP\Client\Enums\Model;
+use GrokPHP\Client\Exceptions\GrokException;
+use GrokPHP\Laravel\Support\GrokResponse;
+
+class GrokVision
+{
+    protected GrokClient $client;
+
+    public function __construct(GrokClient $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Analyze an image using Grok Vision models.
+     * @throws GrokException
+     */
+    public function analyze(string $imagePath, string $prompt, ?Model $model = null): GrokResponse
+    {
+        $response = $this->client->vision()->analyze($imagePath, $prompt, $model);
+
+        return new GrokResponse($response);
+    }
+}
