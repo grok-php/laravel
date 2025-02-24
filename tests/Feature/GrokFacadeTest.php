df2e1f2720574a8f91ed98ecb8e22a5cb7d88da3
Muhammed ElFeqy <thefeqy@gmail.com>
2025-02-25 01:56:41 +0200

    test: Resolve facade correctely

--- tests/Feature/GrokFacadeTest.php
+++ tests/Feature/GrokFacadeTest.php
@@ -2,16 +2,13 @@
 
 namespace Tests\Feature;
 
+use GrokPHP\Laravel\Services\GrokAI;
 use Tests\TestCase;
-use GrokPHP\Laravel\Facades\GrokAI;
-use GrokPHP\Client\Config\ChatOptions;
-use GrokPHP\Client\Enums\Model;
-use Illuminate\Support\Facades\Http;
 
 class GrokFacadeTest extends TestCase
 {
-    public function test_it_sends_a_chat_request_and_receives_a_response()
+    public function test_facade_resolves_correctly()
     {
-        // soon ...
+        $this->assertInstanceOf(GrokAI::class, app('grok-ai'));
     }
 }
